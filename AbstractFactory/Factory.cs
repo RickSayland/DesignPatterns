582311a13a8a5aec81f2cc43e0c2a3b532839e89
rsayl <rsayl@RICKDESKTOP>
2019-08-12 18:15:27 -0400

    Add project files.

---
+++ AbstractFactory/Factory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public class WoodFactory : IFactory
+    {
+        public void MakeDoor(int count)
+        {
+            Console.WriteLine("{0} WOOD doors made!", count);
+        }
+
+        public void MakeFloor(int area)
+        {
+            Console.WriteLine("{0} sized WOOD floor", area);
+        }
+
+        public void MakeWall(int length, int width)
+        {
+            Console.WriteLine("{0}x{1} WOOD wall made!", length, width);
+        }
+    }
+    public class StoneFactory : IFactory
+    {
+        public void MakeDoor(int count)
+        {
+            Console.WriteLine("{0} STONE doors made!", count);
+        }
+
+        public void MakeFloor(int area)
+        {
+            Console.WriteLine("{0} sized STONE floor", area);
+        }
+
+        public void MakeWall(int length, int width)
+        {
+            Console.WriteLine("{0}x{1} STONE wall made!", length, width);
+        }
+    }
+    public class GlassFactory : IFactory
+    {
+        public void MakeDoor(int count)
+        {
+            Console.WriteLine("{0} GLASS doors made!", count);
+        }
+
+        public void MakeFloor(int area)
+        {
+            Console.WriteLine("{0} sized GLASS floor", area);
+        }
+
+        public void MakeWall(int length, int width)
+        {
+            Console.WriteLine("{0}x{1} GLASS wall made!", length, width);
+        }
+    }
+}
---
+++ AbstractFactory/IFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public interface IFactory
+    {
+        void MakeDoor(int count);
+        void MakeWall(int length, int width);
+        void MakeFloor(int area);
+    }
+}
